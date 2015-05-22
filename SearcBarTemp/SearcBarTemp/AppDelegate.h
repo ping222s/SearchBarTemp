889f4c09ae6116bf56d301b52e8628e271c71640
ping222s <zhoupingshuang@zhoupingshuangtekiMacBook-Pro.local>
2015-05-22 20:45:22 +0800

    sdfsf
    sdfsdf

---
+++ SearcBarTemp/SearcBarTemp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearcBarTemp
+//
+//  Created by zhoupingshuang on 15/5/22.
+//  Copyright (c) 2015年 Hangzhou Badian Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearcBarTemp/SearcBarTemp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearcBarTemp
+//
+//  Created by zhoupingshuang on 15/5/22.
+//  Copyright (c) 2015年 Hangzhou Badian Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
